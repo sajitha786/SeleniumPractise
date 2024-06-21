8264ec70fd4720fdb7ddaf6ba73883ee6cf4d34f
Sajitha <ssajitha7@gmail.com>
2024-06-22 01:52:56 +0400

    Handling cookies and taking screenshots

--- src/MaxWindowAndDeleteCookies.java
+++ src/MaxWindowAndDeleteCookies.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MaxWindowAndDeleteCookies {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
@@ -12,5 +18,12 @@ public class MaxWindowAndDeleteCookies {
 		//click on any link, it will redirect to login page
 		//As the session is deleted. thus we can verify login functionality
 		driver.get("https://www.google.com/");
+		
+		//takingScreenshots
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("D:\\Screenshots\\screenshot1.png"));
+		
+		Thread.sleep(2000);
+		driver.close();
 	}
 }
